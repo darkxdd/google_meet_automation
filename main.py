582def682d3f718baf29a6348804fb17a9a149d1
puneeth <puneethcool390@gmail.com>
2024-11-13 01:18:35 +0530

    fix bugs and add fallback

--- main.py
+++ main.py
@@ -1,71 +1,103 @@
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import pytz
+from datetime import datetime
+import getpass
 
+def Glogin(driver, mail_address, password):
+    driver.get('https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAAQ')
+    driver.find_element(By.ID, "identifierId").send_keys(mail_address)
+    driver.find_element(By.ID, "identifierNext").click()
+    driver.implicitly_wait(10)
+    driver.find_element(By.XPATH, '//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
+    driver.implicitly_wait(10)
+    time.sleep(2)
+    driver.find_element(By.ID, "passwordNext").click()
+    driver.implicitly_wait(10)
+    driver.get('https://accounts.google.com/')
+    driver.implicitly_wait(10)
 
-def Glogin(mail_address, password):
+def turnOffMicCam(driver):
+    time.sleep(2)
+    try:
+        mic_button = driver.find_element(By.XPATH, "//div[@aria-label='Turn off microphone']")
+        if mic_button:
+            mic_button.click()
+            print("Microphone turned off.")
+    except Exception:
+        print("Microphone button not found or already muted.")
+
+    time.sleep(1)
+    try:
+        camera_button = driver.find_element(By.XPATH, "//div[@aria-label='Turn off camera']")
+        if camera_button:
+            camera_button.click()
+            print("Camera turned off.")
+    except Exception:
+        print("Camera button not found or already off.")
     
-	driver.get(
-		'https://accounts.google.com/ServiceLogin?hl=en&passive=true&continue=https://www.google.com/&ec=GAZAAQ')
-
-	
-	driver.find_element(By.ID, "identifierId").send_keys(mail_address)
-	driver.find_element(By.ID, "identifierNext").click()
-	driver.implicitly_wait(10)
-
-	
-	driver.find_element(By.XPATH,
-		'//*[@id="password"]/div[1]/div/div[1]/input').send_keys(password)
-	driver.implicitly_wait(10)
-	driver.find_element(By.ID, "passwordNext").click()
-	driver.implicitly_wait(10)
-
-	
-	driver.get('https://google.com/')
-	driver.implicitly_wait(100)
+    time.sleep(1)
+    try:
+        join_button = driver.find_element(By.XPATH, "//span[text()='Join now']")
+        join_button.click()
+        print("Joined the meeting.")
+    except Exception:
+        print("Join button not found.")
+        try:
+            ask_to_join_button = driver.find_element(By.XPATH, "//span[text()='Ask to join']")
+            ask_to_join_button.click()
+            print("Requested to join the meeting.")
+        except Exception:
+            print("Neither 'Join now' nor 'Ask to join' button was found.")
 
 
-def turnOffMicCam():
+def joinNow(driver, meeting_link, join_time_ist, exit_time_ist):
+    
+    ist = pytz.timezone('Asia/Kolkata')
+    join_time_utc = ist.localize(join_time_ist).astimezone(pytz.utc)
+    exit_time_utc = ist.localize(exit_time_ist).astimezone(pytz.utc)
 
-	time.sleep(2)
-	driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div[14]/div[3]/div/div[2]/div[4]/div/div/div[1]/div[1]/div/div[6]/div[1]/div/div/div[1]').click()
+    
+    wait_time = (join_time_utc - datetime.now(pytz.utc)).total_seconds()
+    if wait_time > 0:
+        time.sleep(wait_time)
+
+    
+    driver.get(meeting_link)
+    turnOffMicCam(driver)
+    print("Meeting started.")
+
+    
+    wait_time = (exit_time_utc - datetime.now(pytz.utc)).total_seconds()
+    if wait_time > 0:
+        time.sleep(wait_time)
+
+    driver.quit()
+    print("Meeting ended. Chrome tab closed.")
 
 
-	time.sleep(1)
-	driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div[14]/div[3]/div/div[2]/div[4]/div/div/div[1]/div[1]/div/div[6]/div[2]/div/div[1]').click()
-
-
-
-def joinNow():
-	
-	print(1)
-	time.sleep(5)
-	driver.implicitly_wait(2000)
-	driver.find_element(By.XPATH,
-		'//*[@id="yDmH0d"]/c-wiz/div/div/div[14]/div[3]/div/div[2]/div[4]/div/div/div[2]/div[1]/div[2]/div[1]/div[1]/button').click()
-	print(1)
-
-mail_address = 'YOUR_EMAIL'
-password = 'EMAIL_PASSWORD'
+today = datetime.now().date()
+join_time_ist = datetime.combine(today, datetime.strptime(input("Enter the start time (IST) in the format HH:MM:SS: "), '%H:%M:%S').time())
+exit_time_ist = datetime.combine(today, datetime.strptime(input("Enter the end time (IST) in the format HH:MM:SS: "), '%H:%M:%S').time())
+mail_address = input("Enter your email address: ")
+password = getpass.getpass("Enter your password: ")
+meeting_link = input("Enter the meeting link: ")
 
 
 opt = Options()
 opt.add_argument('--disable-blink-features=AutomationControlled')
 opt.add_argument('--start-maximized')
 opt.add_experimental_option("prefs", {
-	"profile.default_content_setting_values.media_stream_mic": 1,
-	"profile.default_content_setting_values.media_stream_camera": 1,
-	"profile.default_content_setting_values.geolocation": 0,
-	"profile.default_content_setting_values.notifications": 1
+    "profile.default_content_setting_values.media_stream_mic": 1,
+    "profile.default_content_setting_values.media_stream_camera": 1,
+    "profile.default_content_setting_values.geolocation": 0,
+    "profile.default_content_setting_values.notifications": 1
 })
+
+
 driver = webdriver.Chrome(options=opt)
-
-
-Glogin(mail_address, password)
-
-
-driver.get('https://meet.google.com/MEETING_ID')
-turnOffMicCam()
-
-joinNow()
+Glogin(driver, mail_address, password)
+joinNow(driver, meeting_link, join_time_ist, exit_time_ist)
